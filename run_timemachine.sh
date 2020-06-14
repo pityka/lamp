d4a0fe33543889b0beef0c9fe281da66c99e6c82
Istvan Bartha <bartha.pityu@gmail.com>
2020-06-14 20:43:36 +0200

    Edit run_timemachine.sh

--- run_timemachine.sh
+++ run_timemachine.sh
@@ -3,6 +3,6 @@
 TOKEN=$(git config --global --get github.token)
 
 id=$(docker --context vm1 build -q .) && \
-echo $id && docker --context vm1 run --mount type=volume,source=data1,destination=/data \
+echo $id && docker --context vm1 run -d --mount type=volume,source=data1,destination=/data \
 --env GITHUB_TOKEN=$TOKEN --gpus all $id /bin/bash -c \
-"sbt 'example_timemachine/run --gpu --train-data /data/time_machine/train.txt --test-data /data/time_machine/test.txt --epochs 20000 --learning-rate 0.0001 --single --train-batch 1024 --validation-batch 128 --checkpoint-save checkpoint'"
+"sbt 'example_timemachine/run --gpu --train-data /data/time_machine/train.txt --test-data /data/time_machine/test.txt --epochs 20000 --learning-rate 0.00001 --single --train-batch 1024 --validation-batch 128 --checkpoint-save checkpoint'"
