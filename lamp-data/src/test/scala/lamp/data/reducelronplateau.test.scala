ded6f38c288e507fd25f5d05bf3e7c466ff87983
Istvan Bartha <bartha.pityu@gmail.com>
2021-05-26 08:16:25 +0200

    Make test compile

--- lamp-data/src/test/scala/lamp/data/reducelronplateau.test.scala
+++ lamp-data/src/test/scala/lamp/data/reducelronplateau.test.scala
@@ -6,15 +6,27 @@ import lamp.nn.LearningRateSchedule
 class ReduceLROnPlateauSuite extends AnyFunSuite {
   test("reduce lr test") {
     val lrs = LearningRateSchedule.reduceLROnPlateau(stopFactor = 0.1)
-    assert(lrs.learningRateFactor(0L, Some(10d)) == 1d)
-    assert(lrs.learningRateFactor(1L, Some(10d)) == 1d)
-    assert(lrs.learningRateFactor(9L, Some(10d)) == 1d)
-    assert(lrs.learningRateFactor(10L, Some(10d)) == 0.5)
-    assert(lrs.learningRateFactor(18L, Some(11d)) == 0.5)
-    assert(lrs.learningRateFactor(19L, Some(9d)) == 0.5)
-    assert(lrs.learningRateFactor(28L, Some(9d - 1e-6)) == 0.5)
-    assert(lrs.learningRateFactor(29L, Some(9d - 1e-6)) == 0.25)
-    assert(lrs.learningRateFactor(39L, Some(9d - 1e-6)) == 0.125)
-    assert(lrs.learningRateFactor(50L, Some(9d - 1e-6)) == 0.0)
+    val st = lrs.init
+
+    import cats.data.State
+    def next(e: Long, d: Double, exp: Double) =
+      State(st => lrs.learningRateFactor(st, e, Some(d))).map { b =>
+        assert(b == exp)
+        b
+      }
+
+    (for {
+      _ <- next(0L, 10d, 1d)
+      _ <- next(1L, 10d, 1d)
+      _ <- next(9L, 10d, 1d)
+      _ <- next(10L, 10d, 0.5)
+      _ <- next(18L, 11d, 0.5)
+      _ <- next(19L, 9d, 0.5)
+      _ <- next(28L, 9d - 1e-6, 0.5)
+      _ <- next(29L, 9d - 1e-6, 0.25)
+      _ <- next(39L, 9d - 1e-6, 0.125)
+      _ <- next(50, 9d - 1e-6, 0.0)
+    } yield ()).run(st).value
+
   }
 }
