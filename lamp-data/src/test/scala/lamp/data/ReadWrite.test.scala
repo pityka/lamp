1a5d56d97678eb23e43eaf6e81a282e4f8df0333
Istvan Bartha <bartha.pityu@gmail.com>
2021-09-07 10:13:10 +0200

    empty tensors from file

--- lamp-data/src/test/scala/lamp/data/ReadWrite.test.scala
+++ lamp-data/src/test/scala/lamp/data/ReadWrite.test.scala
@@ -17,10 +17,29 @@ class ReadWriteSuite extends AnyFunSuite {
       val file = File.createTempFile("prefix", "suffx")
       val st1 = STen.ones(List(3, 3))
       val st2 = STen.zeros(List(0))
-      val st3 = STen.zeros(List(0), STenOptions.f)
+      val st3 = STen.zeros(List(0, 0), STenOptions.f)
       val st4 = STen.zeros(List(0), STenOptions.l)
       val st5 = STen.ones(List(3, 3))
       val list = List(st1, st2, st3, st4, st5)
+      assert(
+        Writer
+          .writeTensorsIntoFile(list, file)
+          .unsafeRunSync()
+          .isRight
+      )
+      val read1 = Reader.readTensorsFromFile(file, lamp.CPU, false)
+      val read2 = Reader.readTensorsFromFile(file, lamp.CPU, true)
+      list.zip(read1).foreach(a => assert(a._1.equalDeep(a._2)))
+      list.zip(read2).foreach(a => assert(a._1.equalDeep(a._2)))
+    }
+  }
+  test("io empty 2") {
+    Scope.root { implicit scope =>
+      val file = File.createTempFile("prefix", "suffx")
+      val st2 = STen.zeros(List(0))
+      val st3 = STen.zeros(List(0, 0), STenOptions.f)
+      val st4 = STen.zeros(List(0), STenOptions.l)
+      val list = List(st2, st3, st4)
       assert(
         Writer
           .writeTensorsIntoFile(list, file)
