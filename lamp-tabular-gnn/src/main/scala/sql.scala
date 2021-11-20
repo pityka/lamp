ccf0f4467694badfdb2405d95603a335f7c5915a
Istvan Bartha <bartha.pityu@gmail.com>
2021-11-20 19:58:57 +0100

    wip

--- lamp-tabular-gnn/src/main/scala/sql.scala
+++ lamp-tabular-gnn/src/main/scala/sql.scala
@@ -11,27 +11,32 @@ object Sql {
   val underscore = Parser.charIn('_')
   val identifierPart = Rfc5234.alpha | Rfc5234.digit
 
-  val identifier = (identifierStart ~ (underscore | identifierPart ).rep0).map{ case (s,l) => (s::l).mkString} 
+  val identifier =
+    (identifierStart ~ (underscore.backtrack | identifierPart).rep0).map {
+      case (s, l) => (s :: l).mkString
+    }
 
+  val wh = Rfc5234.wsp.rep
+  val optionalWh = Rfc5234.wsp.rep0
   val period = Parser.char('.')
   val quote = Parser.char('\'')
   val asterisk = Parser.char('*')
   val comma = Parser.char(',')
-  val qualifiedIdentifier = (identifier ~ period ~ identifier).map{ case ((q,_),i) => (q,i)}
+  val open = Parser.char('(')
+  val close = Parser.char(')')
+  val qualifiedIdentifier = ((identifier.soft <* period).?.with1 ~ identifier)
 
   val numericLiteral = Numbers.jsonNumber
   val stringLiteral = (quote *> Parser.anyChar.rep0 <* quote).map(_.mkString)
   val literal = numericLiteral | stringLiteral
 
-  val distinct = Parser.string("distinct").rep0(0,1).map(_.size > 0)
+  val distinct = (Parser.string("distinct") <* wh).?.map(_.size > 0)
 
-  val columnReference = identifier | qualifiedIdentifier
+  val columnReference = qualifiedIdentifier
   val asClause = Parser.string("as") *> identifier
 
-  val wh = Rfc5234.wsp.rep
-
   val valueExpression = columnReference
-  
+
   val derivedColumn = valueExpression ~ asClause.rep0
   val selectSublist = identifier ~ period ~ asterisk | derivedColumn
 
@@ -39,9 +44,13 @@ object Sql {
 
   // va
 
-  val fromClause =   Parser.string("from") *> wh *> identifier.repSep(comma.surroundedBy(wh))
+  val fromClause = Parser.string("from") *> wh *> identifier.repSep(
+    comma.soft.surroundedBy(optionalWh)
+  )
 
-  val joinType = Parser.string("inner") | Parser.string("left") | Parser.string("right") | Parser.string("full") 
+  val joinType = Parser.string("inner") | Parser.string("left") | Parser.string(
+    "right"
+  ) | Parser.string("full")
 
   val or = Parser.string("or")
   val and = Parser.string("and")
@@ -53,24 +62,40 @@ object Sql {
   val equalsOp = Parser.string("=")
   val booleanValue = bTrue | bFalse | bNull
 
-  trait BooleanAST
+  class BooleanAST
 
-  val comparisonOperator = equalsOp 
-  val comparisonPredicate = columnReference ~ wh ~ comparisonOperator ~ wh ~ columnReference
-  val predicate = comparisonPredicate 
-  val booleanPrimary = predicate 
-  val isnot = is ~ wh ~ not.rep0 ~ wh ~ booleanValue
-  val booleanTest = booleanPrimary ~ isnot.rep0
-  val booleanFactor  : Parser[BooleanAST] = (not.rep0.with1 ~ wh ~ booleanTest).map(_.asInstanceOf[BooleanAST])
-  val booleanTerm : Parser[BooleanAST] = Parser.recursive[BooleanAST]( recurse => booleanFactor | (recurse ~ wh ~ and ~ wh ~ booleanFactor).map(_.asInstanceOf[BooleanAST]))
-  val booleanExpression = Parser.recursive[BooleanAST]( recurse => booleanTerm | (recurse ~ wh ~ or ~ wh ~ booleanTerm).map(_ => ???))
+  val comparisonOperator = equalsOp
+  val comparisonPredicate =
+    columnReference ~ wh ~ comparisonOperator ~ wh ~ columnReference
+  val predicate = comparisonPredicate
+  val booleanPrimary = predicate
+  val booleanTest = booleanPrimary
 
-  val joinedTable = joinType.rep0.with1 ~ wh ~ Parser.string("join") ~ wh ~ identifier ~ wh ~ Parser.string("on") ~ wh ~ booleanExpression
+  val booleanExpression: Parser[BooleanAST] = Parser.recursive[BooleanAST] {
+    recurse =>
+      val booleanFactor =
+        (open ~ optionalWh *> recurse <* optionalWh.soft ~ close).backtrack  |
+        booleanTest
 
-  val tableExpression = fromClause ~ wh ~ joinedTable.rep0 //~ whereClause.rep0 ~ groupByClause.rep0 ~ havingClause.rep0
+        // (not ~ wh).void.?.map(
+        //   _.isDefined
+        // ).with1 ~ 
+        
+      val booleanTerm = booleanFactor ~ (wh.soft *> and <* wh ~ booleanFactor).rep0
 
-  val query = Parser.string("select") *> wh *> distinct ~ wh ~ selectList ~ wh ~ tableExpression
+      (booleanTerm ~ (wh.soft *> or <* wh ~ booleanTerm).rep0).map(_ => new BooleanAST)
+  }
   
-  
+
+  val joinedTable =
+    (joinType ~ wh).? ~ Parser.string("join") ~ wh ~ identifier ~ wh ~ Parser
+      .string("on") ~ wh ~ booleanExpression
+
+  val tableExpression =
+    fromClause ~ (wh ~ joinedTable).? //~ whereClause.rep0 ~ groupByClause.rep0 ~ havingClause.rep0
+
+  val query = Parser.string(
+    "select"
+  ) *> wh *> distinct ~ selectList ~ wh ~ tableExpression
 
 }
--- lamp-tabular-gnn/src/test/scala/sql.test.scala
+++ lamp-tabular-gnn/src/test/scala/sql.test.scala
@@ -4,6 +4,6 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class SQLSuite extends AnyFunSuite {
   test("dfs"){
-    println(Sql.query.parse(""))
+    println(Sql.query.parse("select * from table1 inner join table2 on table.id = table.id and ( table.id = table.id or table.id = table.id ) and table.id = table.id"))
   }
 }
