6f1d50ba20b11b968fb05cc8b10cf585eeea4c85
Scala Steward <me@scala-steward.org>
2022-04-08 12:46:13 +0200

    Reformat with scalafmt 3.4.3

--- lamp-data/src/main/scala/lamp/data/IOLoops.scala
+++ lamp-data/src/main/scala/lamp/data/IOLoops.scala
@@ -732,7 +732,8 @@ object IOLoops {
       _ <- IO {
         logger.foreach(
           _.info(
-            s"Avg training loss in epoch $epochCount over $numInstances examples: $trainingLoss (${"%.2f".format(throughput)} instances/sec)"
+            s"Avg training loss in epoch $epochCount over $numInstances examples: $trainingLoss (${"%.2f"
+                .format(throughput)} instances/sec)"
           )
         )
       }
