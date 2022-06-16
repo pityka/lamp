c368ce67f8c673553fc59c0799d9a8bd11704c4b
Istvan Bartha <106101+pityka@users.noreply.github.com>
2022-06-16 11:55:31 +0200

    Add diagonal Shampoo Skip every N iteratios with svd Add momentum to Shampoo

--- lamp-core/src/main/scala/lamp/nn/Shampoo.scala
+++ lamp-core/src/main/scala/lamp/nn/Shampoo.scala
@@ -9,14 +9,20 @@ object Shampoo {
   def factory(
       learningRate: OptimizerHyperparameter = simple(0.001),
       clip: Option[Double] = None,
-      eps: Double = 1e-4
+      eps: Double = 1e-4,
+      diagonalThreshold: Int = 256,
+      updatePreconditionerEveryNIterations: Int = 100,
+      momentum: OptimizerHyperparameter = simple(0d)
   ) =
     (parameters: Seq[(STen, PTag)]) =>
       Shampoo(
         parameters,
         learningRate,
         clip,
-        eps
+        eps,
+        diagonalThreshold,
+        updatePreconditionerEveryNIterations,
+        momentum
       )
 }
 
@@ -27,19 +33,35 @@ case class Shampoo(
     parameters: Seq[(STen, PTag)],
     learningRate: OptimizerHyperparameter = simple(0.001),
     clip: Option[Double] = None,
-    eps: Double = 1e-4
+    eps: Double = 1e-4,
+    diagonalThreshold: Int = 256,
+    updatePreconditionerEveryNIterations: Int = 100,
+    momentum: OptimizerHyperparameter = simple(0d)
 ) extends Optimizer {
   val scope = Scope.free
-  val lt: List[STen] = parameters.toList.map { case (param, _) =>
+
+  val lt: List[(STen, STen)] = parameters.toList.map { case (param, _) =>
     val dim1 = param.shape(0)
-    val t = STen.eye(dim1.toInt, param.options(scope))(scope)
+    val t =
+      if (dim1 > 512) STen.ones(List(dim1.toInt), param.options(scope))(scope)
+      else
+        STen.eye(dim1.toInt, param.options(scope))(scope)
     t *= eps
-    t
+    val ltinv = t.cloneTensor(scope)
+    (t, ltinv)
   }
-  val rt: List[STen] = parameters.toList.map { case (param, _) =>
+  val rt: List[(STen, STen)] = parameters.toList.map { case (param, _) =>
     val dim2 = param.shape.drop(1).foldLeft(1L)(_ * _)
-    val t = STen.eye(dim2.toInt, param.options(scope))(scope)
+    val t =
+      if (dim2 > 512) STen.ones(List(dim2.toInt), param.options(scope))(scope)
+      else
+        STen.eye(dim2.toInt, param.options(scope))(scope)
     t *= eps
+    (t, t.cloneTensor(scope))
+  }
+  val lastGradient: List[STen] = parameters.toList.map { case (param, _) =>
+    val t =
+      STen.zerosLike(param)(scope)
     t
   }
 
@@ -53,7 +75,10 @@ case class Shampoo(
 
   var stepCount = 0L
   val stepCountSTen = STen.scalarDouble(0, STenOptions.d)(scope)
-  def state = List(stepCountSTen) ++ lt ++ rt
+  def state =
+    List(stepCountSTen) ++ lt.flatMap(v => List(v._1, v._2)) ++ rt.flatMap(v =>
+      List(v._1, v._2)
+    ) ++ lastGradient
   def release() = {
     scope.release()
   }
@@ -65,36 +90,100 @@ case class Shampoo(
       .zip(gradients)
       .zip(lt)
       .zip(rt)
-      .filter(_._1._1._2.isDefined)
+      .zip(lastGradient)
+      .filter(_._1._1._1._2.isDefined)
       .foreach {
-        case ((((_, _), None), _), _) =>
+        case (((((_, _), None), _), _), _) =>
           // won't happent, see filter above
           ???
-        case ((((param, tag), Some(gradients)), lt), rt) =>
+        case (
+              ((((param, tag), Some(gradients)), (lt, ltinv)), (rt, rtinv)),
+              lastGradient
+            ) =>
           val lr = learningRate(tag)
+          val mom = momentum(tag)
+
+          if (stepCount > 0 && mom > 0) {
+            gradients *= (1d - mom)
+            STen.addOut(gradients, gradients, lastGradient, mom)
+          }
+          if (mom > 0) {
+            lastGradient.copyFrom(gradients)
+          }
 
           Scope.root { implicit sc =>
             val gradientsAsMatrix = gradients.view(gradients.shape(0), -1L)
-            lt += gradientsAsMatrix.mm(gradientsAsMatrix.t(scope))
 
-            rt += (gradientsAsMatrix.t.mm(gradientsAsMatrix))
-
-            val ltinv14 = {
-              val (u, s, vt) = lt.castToDouble.svd()
-              val s2 = (s).pow(-0.25)
-              (u * s2.view(-1, 1)).mm(vt).castToType(gradients.scalarTypeByte)
+            if (lt.shape.size == 2) {
+              lt += gradientsAsMatrix.mm(gradientsAsMatrix.t(scope))
+            } else {
+              lt += (gradientsAsMatrix * gradientsAsMatrix).sum(
+                dim = 1,
+                keepDim = false
+              )
             }
 
-            val rtinv14 = {
-              val (u, s, vt) = rt.castToDouble.svd(false)
-              val s2 = (s).pow(-0.25)
-              (u * s2.view(-1, 1)).mm(vt).castToType(gradients.scalarTypeByte)
+            if (rt.shape.size == 2) {
+              rt += (gradientsAsMatrix.t.mm(gradientsAsMatrix))
+            } else {
+              rt += (gradientsAsMatrix * gradientsAsMatrix).sum(
+                dim = 0,
+                keepDim = false
+              )
             }
 
-            val m = ltinv14
-              .mm(gradientsAsMatrix)
-              .mm(rtinv14)
-              .view(gradients.shape: _*)
+            val ltinv14 =
+              if (
+                stepCount > 100 && stepCount % updatePreconditionerEveryNIterations != 0
+              )
+                ltinv
+              else {
+                val updated = if (lt.shape.size == 2) {
+                  val (u, s, vt) = lt.castToDouble.svd()
+                  val s2 = (s).pow(-0.25)
+                  (u * s2
+                    .view(-1, 1)).mm(vt).castToType(gradients.scalarTypeByte)
+                } else {
+                  lt.pow(-0.25)
+                }
+                ltinv.copyFrom(updated)
+
+                updated
+              }
+
+            val rtinv14 =
+              if (
+                stepCount > 100 && stepCount % updatePreconditionerEveryNIterations != 0
+              ) rtinv
+              else {
+                val updated =
+                  if (rt.shape.size == 2) {
+                    val (u, s, vt) = rt.castToDouble.svd()
+                    val s2 = (s).pow(-0.25)
+                    (u * s2
+                      .view(-1, 1)).mm(vt).castToType(gradients.scalarTypeByte)
+                  } else {
+                    rt.pow(-0.25)
+                  }
+                rtinv.copyFrom(updated)
+
+                updated
+              }
+
+            val m = {
+              val t1 =
+                if (lt.shape.size == 2)
+                  ltinv14
+                    .mm(gradientsAsMatrix)
+                else ltinv14.unsqueeze(1) * gradientsAsMatrix
+
+              val t2 =
+                if (rt.shape.size == 2)
+                  t1.mm(rtinv14)
+                else t1 * rtinv14.unsqueeze(0)
+
+              t2.view(gradients.shape: _*)
+            }
 
             ATen.add_out(
               param.value,
--- lamp-core/src/test/scala/lamp/nn/shampoo.test.scala
+++ lamp-core/src/test/scala/lamp/nn/shampoo.test.scala
@@ -4,6 +4,7 @@ import org.saddle._
 import org.scalatest.funsuite.AnyFunSuite
 import lamp.saddle._
 import lamp.Scope
+import lamp.STen
 
 class ShampooSuite extends AnyFunSuite {
 
@@ -32,6 +33,28 @@ class ShampooSuite extends AnyFunSuite {
       assert(
         updatedParams2.roundTo(4) == Mat(Vec(0.9053, -0.4542)).T
       )
+    }
+  }
+  test1("Shampoo without weight decay - diagonal") { cuda =>
+    Scope.root { implicit scope =>
+      val initParams = mat.ones(1, 513)
+      val params = lamp.saddle.fromMat(initParams, cuda)
+      val gradients = STen.rand(List(1,513), params.options)
+      val opt = Shampoo(
+        parameters = List((params, NoTag)),
+        learningRate = simple(0.1d),
+      )
+      opt.step(List(Some(gradients)), 1d)
+      // val updatedParams1 = params.toMat
+      // assert(
+      //   updatedParams1.roundTo(4) == Mat(Vec(0.9445, 0.2101)).T
+      // )
+      opt.step(List(Some(gradients)), 1d)
+      // val updatedParams2 = params.toMat
+
+      // assert(
+      //   updatedParams2.roundTo(4) == Mat(Vec(0.9053, -0.4542)).T
+      // )
     }
   }
   
