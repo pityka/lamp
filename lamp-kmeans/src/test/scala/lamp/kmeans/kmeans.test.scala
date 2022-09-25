bda00a04a84a8b8dc00c189ab4d9a130d4c1eede
Istvan Bartha <106101+pityka@users.noreply.github.com>
2022-09-25 12:12:06 +0200

    kmeans

--- lamp-kmeans/src/test/scala/lamp/kmeans/kmeans.test.scala
+++ lamp-kmeans/src/test/scala/lamp/kmeans/kmeans.test.scala
@@ -7,6 +7,7 @@ import lamp.nn.CudaTest
 import lamp.Scope
 import lamp.STen
 import lamp.CudaDevice
+import lamp.CPU
 
 class KmeansSuite extends AnyFunSuite {
   def test1(id: String)(fun: Boolean => Unit) = {
@@ -18,41 +19,58 @@ class KmeansSuite extends AnyFunSuite {
     Scope.unsafe { implicit scope =>
       val c1 = STen.randn(List(100, 2))
       val c2 =
-        STen.randn(List(100, 2)) + lamp.saddle.fromMat(Mat(Vec(100d, 100d)).T)
+        STen.randn(List(100, 2)) * 0.1 + lamp.saddle.fromMat(
+          Mat(Vec(100d, 100d)).T
+        )
       val c3 =
-        STen.randn(List(100, 2)) + lamp.saddle.fromMat(Mat(Vec(200d, 200d)).T)
+        STen.randn(List(100, 2)) * 0.1 + lamp.saddle.fromMat(
+          Mat(Vec(200d, 200d)).T
+        )
       val c4 =
-        STen.randn(List(100, 2)) + lamp.saddle.fromMat(Mat(Vec(300d, 300d)).T)
+        STen.randn(List(100, 2)) * 0.1 + lamp.saddle.fromMat(
+          Mat(Vec(300d, 300d)).T
+        )
       val d = STen.cat(List(c1, c2, c3, c4), dim = 0)
       val d2 = if (cuda) CudaDevice(0).to(d) else d
-      val (centers, members, distances) = minibatchKMeans(
-        instances = d2,
-        clusters = 4,
-        iterations = 5,
-        minibatchSize = 10,
-        learningRate = 0.1
-      )
-      val centersM = centers.toMat.rows
+      0 until 100 foreach { _ =>
+        Scope { implicit scope =>
+          val (centers) = minibatchKMeans(
+            instances = d2,
+            clusters = 4,
+            iterations = 100,
+            minibatchSize = 50,
+            learningRate = 0.1,
+            device = CPU
+          )
+          val (members, distances) =
+            assignInstances(instances = d2, centers = centers)
+          val centersM = centers.toMat.rows
 
-      def assertCenter(c: Vec[Double],min:Int,max:Int) = {
-        import org.saddle.ops.BinOps._
+          def assertCenter(c: Vec[Double], min: Int, max: Int) = {
+            import org.saddle.ops.BinOps._
 
-        val p = centersM.zipWithIndex.find { case (row,_) =>
-          val e = row - c
-          val d = math.sqrt((e dot e))
-          d < 10d
+            val p = centersM.zipWithIndex.find { case (row, _) =>
+              val e = row - c
+              val d = math.sqrt((e dot e))
+              d < 10d
+            }
+            assert(p.isDefined)
+            assert(
+              members.toLongVec.slice(min, max).toSeq.forall(_ == p.get._2)
+            )
+
+          }
+
+          assertCenter(Vec(0d, 0d), 0, 100)
+          assertCenter(Vec(100d, 100d), 100, 200)
+          assertCenter(Vec(200d, 200d), 200, 300)
+          assertCenter(Vec(300d, 300d), 300, 400)
+
+          assert(distances.toVec.toSeq.forall(_ < 100d))
+
+          ()
         }
-        assert(p.isDefined)
-        assert(members.toLongVec.slice(min,max).toSeq.forall(_ == p.get._2))
-
       }
-
-      assertCenter(Vec(0d, 0d),0,100)
-      assertCenter(Vec(100d, 100d),100,200)
-      assertCenter(Vec(200d, 200d),200,300)
-      assertCenter(Vec(300d, 300d),300,400)
-
-      assert(distances.toVec.toSeq.forall(_ < 100d))
 
     }
   }
