a83db6994eb51d76d15f159560f29c31e4f04c1e
Istvan Bartha <106101+pityka@users.noreply.github.com>
2023-04-15 18:04:53 +0200

    Fix dimension errors

--- experiment-recursive-lm/src/main/scala/lamp/experiment/recursivelm/model/Loader.scala
+++ experiment-recursive-lm/src/main/scala/lamp/experiment/recursivelm/model/Loader.scala
@@ -8,69 +8,83 @@ object DataLoader {
       minibatchSize: Int,
       numBatches: Int,
       documents: Array[STen],
-      blockLength: Int
+      blockLength: Int,
+      recursionLength: Int
   ) = {
     val N = documents.length
     val rng = scala.util.Random
     def makeNonEmptyBatch(device: Device) = {
       BatchStream.scopeInResource.map { implicit scope =>
         val (tokens, targets) = Scope { implicit scope =>
-          val minibatches = 0 until minibatchSize map {_ =>
-          val doc = documents(rng.nextInt(N))
-          val docLength = doc.shape(0).toInt
+          val minibatches = 0 until minibatchSize map { _ =>
+            val doc = documents(rng.nextInt(N))
+            val docLength = doc.shape(0).toInt
 
-          def makeIndex(n: Int, acc: List[Int]): List[Int] =
-            n match {
-              case 0 => acc.reverse
-              case _ =>
-                acc match {
-                  case head :: _ =>
-                    if (head < docLength - blockLength - 1)
+            def makeIndex(n: Int, acc: List[Int]): List[Int] =
+              n match {
+                case 0 => acc.reverse
+                case _ =>
+                  acc match {
+                    case head :: _ =>
+                      if (head < docLength - blockLength - 1)
+                        makeIndex(
+                          n - 1,
+                          (rng.nextInt(
+                            docLength - blockLength - 1 - head
+                          ) + head) :: acc
+                        )
+                      else acc.reverse
+                    case immutable.Nil =>
                       makeIndex(
                         n - 1,
-                        (rng.nextInt(
-                          docLength - blockLength - 1 - head
-                        ) + head) :: acc
+                        rng.nextInt(
+                          docLength - blockLength - 1
+                        ) :: Nil
                       )
-                    else acc.reverse
-                  case immutable.Nil =>
-                    makeIndex(
-                      n - 1,
-                      rng.nextInt(
-                        docLength - blockLength - 1
-                      ) :: Nil
-                    )
-                }
+                  }
+              }
+            val starts = makeIndex(recursionLength, Nil)
+
+            starts.map { start =>
+              val token = doc
+                .slice(0, start.toLong, start.toLong + blockLength, 1L)
+                .castToLong
+              val target = doc
+                .slice(
+                  0,
+                  start.toLong + 1L,
+                  start.toLong + blockLength + 1L,
+                  1L
+                )
+                .castToLong
+              (token, target)
             }
-          val starts = makeIndex(3,Nil)
-
-          starts.map{ start =>
-            val token = doc.slice(0,start.toLong,start.toLong+blockLength,1L).castToLong  
-            val target = doc.slice(0,start.toLong+1L,start.toLong+blockLength+1L,1L).castToLong  
-            (token,target)
           }
-        }
 
-        val minibatchTokens = minibatches.map(_.map(_._1)).transpose.map(st => STen.stack(st,dim=1))
-        val minibatchTargets = minibatches.map(_.map(_._2)).transpose.map(st => STen.stack(st,dim=1))
-
-          
+          val minibatchTokens = minibatches
+            .map(_.map(_._1))
+            .transpose
+            .map(st => STen.stack(st, dim = 0))
+          val minibatchTargets = minibatches
+            .map(_.map(_._2))
+            .transpose
+            .map(st => STen.stack(st, dim = 0))
 
           device.withOtherStreamThenSync(synchronizeBefore = false) {
 
             (
               minibatchTokens.map(device.to),
-              minibatchTargets.map(device.to),
+              minibatchTargets.map(device.to)
             )
           }
         }
 
-
         val batch = LossInput(
-         tokens.map(const) zip targets
+          tokens.map(const) zip targets
         )
 
-        val fakeTarget = STen.zeros(List(minibatchSize), device.options(SinglePrecision))
+        val fakeTarget =
+          STen.zeros(List(minibatchSize), device.options(SinglePrecision))
 
         NonEmptyBatch((batch, fakeTarget))
       }
