95b66e380f3c26997f91dd4bb30e0c73dd4e34b3
Istvan Bartha <106101+pityka@users.noreply.github.com>
2023-04-15 11:43:17 +0200

    distributed training

--- example-autoregressivelm/src/main/scala/lamp/example/lm/main.scala
+++ example-autoregressivelm/src/main/scala/lamp/example/lm/main.scala
@@ -6,6 +6,9 @@ import cats.effect.IO
 import cats.effect.ExitCode
 import cats.effect.IOApp
 
+import lamp.example.lm.DistributedTrain
+import lamp.example.lm.Train
+
 object Main extends IOApp {
   scribe.info("Logger start")
 
@@ -16,7 +19,10 @@ object Main extends IOApp {
         Scope.inResource.use(scope =>
           for {
             corpora <- Util.prepareCorpora(config)(scope)
-            _ <- Train.train(config, corpora._1, corpora._2)(scope)
+            _ <-
+              if (!config.distributed)
+                Train.train(config, corpora._1, corpora._2)(scope)
+              else DistributedTrain.train(config, corpora._1, corpora._2)(scope)
           } yield ExitCode(0)
         )
 
@@ -35,119 +41,3 @@ object Main extends IOApp {
     }
 
 }
-
-// if (config.distributed) {
-//         Scope.root { implicit scope =>
-//           scribe.info(
-//             s"Distributed training rank: ${config.rank} nrank:${config.nranks} gpu:${config.gpu}"
-//           )
-//           val device = CudaDevice(config.gpu)
-//           val model = allocateModel(device)
-
-//           val actorSystem = akka.actor.ActorSystem(
-//             name = s"lm-${config.rank}",
-//             config = Some(
-//               com.typesafe.config.ConfigFactory.parseString(
-//                 s"""
-// akka {
-//   actor {
-//     provider = remote
-//   }
-//   remote {
-//     artery {
-//       transport = tcp
-//       canonical.hostname = "${config.myAddress}"
-//       canonical.port = ${config.myPort}
-//     }
-//   }
-// }
-//                 """
-//               )
-//             )
-//           )
-
-//           val trainEpochs = () =>
-//             lamp.data.languagemodel
-//               .autoregressiveMinibatchesFromCorpus(
-//                 minibatchSize = config.trainBatchSize,
-//                 numBatches = config.numBatchesPerEpoch,
-//                 corpus = trainCorpus,
-//                 blockLength = contextLength
-//               )
-//               .withoutEmptyBatches
-//               .everyNth(n = config.nranks, offset = config.rank)
-
-//           val validEpochs = () =>
-//             lamp.data.languagemodel
-//               .autoregressiveMinibatchesFromCorpus(
-//                 minibatchSize = config.trainBatchSize,
-//                 numBatches = config.numBatchesPerEpoch,
-//                 corpus = validCorpus,
-//                 blockLength = contextLength
-//               )
-//               .withoutEmptyBatches
-//               .everyNth(n = config.nranks, offset = config.rank)
-
-//           val program = if (config.rank == 0) {
-
-//             val optimizer = AdamW.factory(
-//               weightDecay = simple(config.weightDecay),
-//               learningRate = simple(config.learningRate),
-//               beta2 = simple(config.beta2),
-//               clip = Some(1d)
-//             )
-
-//             // val checkpointedState = config.checkpointLoad.map { state =>
-//             //   StateIO
-//             //     .readFromFile(new File(state), device)
-//             //     .asInstanceOf[SimpleLoopState]
-//             // }
-
-//             val comm = new lamp.distributed.akka.AkkaCommunicationServer(
-//               actorSystem
-//             )
-//             lamp.data.distributed.driveDistributedTraining(
-//               nranks = config.nranks,
-//               gpu = config.gpu,
-//               controlCommunication = comm,
-//               model = model,
-//               optimizerFactory = optimizer,
-//               trainBatches = trainEpochs,
-//               validationBatches = validEpochs,
-//               maxEpochs = config.epochs
-//               // initState = checkpointedState,
-//               // checkpointState = Some((state: LoopState, _: Unit) =>
-//               //   config.checkpointSave
-//               //     .map { file =>
-//               //       StateIO.stateToFile(new File(file))(state)
-//               //     }
-//               //     .getOrElse(IO.unit)
-//               // )
-//             )
-
-//           } else {
-//             import scala.concurrent.duration._
-//             val comm = new lamp.distributed.akka.AkkaCommunicationClient(
-//               actorSystem,
-//               config.rootAddress,
-//               config.rootPort,
-//               "cifar-0",
-//               600 seconds
-//             )
-
-//             lamp.data.distributed.followDistributedTraining(
-//               rank = config.rank,
-//               nranks = config.nranks,
-//               gpu = config.gpu,
-//               controlCommunication = comm,
-//               model = model,
-//               trainBatches = trainEpochs,
-//               validationBatches = validEpochs
-//             )
-
-//           }
-
-//           program.unsafeRunSync()
-//           actorSystem.terminate()
-//           ()
-//         }
