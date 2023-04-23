4bfd6d5775809760a179d12b1716c0e1e57457e4
Istvan Bartha <106101+pityka@users.noreply.github.com>
2023-04-23 19:08:33 +0200

    Make bpe test pass

--- lamp-data/src/test/scala/lamp/data/bytesegmentencoding/bpe.test.scala
+++ lamp-data/src/test/scala/lamp/data/bytesegmentencoding/bpe.test.scala
@@ -5,7 +5,8 @@ import org.scalatest.funsuite.AnyFunSuite
 class ByteSegmentEncodingSuite extends AnyFunSuite {
   test("byte segment encoding") {
     val corpus =
-      Array.apply[Byte](0, 0, 1, 2, 0, 1, 2, 1, 4, 2, 5, 2, 6, 9, 9, 9)
+      Array.apply[Byte]('0', '0', '1', '2', '0', '1', '2', '1', '4', '2', '5',
+        '2', '6', '9', '9', '9')
     val trained =
       lamp.data.bytesegmentencoding.train(corpus, 0.toChar, 100.toChar, 3)
     val encoded =
@@ -14,39 +15,39 @@ class ByteSegmentEncodingSuite extends AnyFunSuite {
       lamp.data.bytesegmentencoding.decode(encoded, trained, '?'.toByte)
     assert(corpus.toVector == decoded.toVector)
     assert(
-      trained == Vector(
-        (Vector(5), 0),
-        (Vector(0), 1),
-        (Vector(4), 2),
-        (Vector(6), 3),
-        (Vector(1), 4),
-        (Vector(2), 5),
-        (Vector(9), 6),
-        (Vector(0, 1, 2), 7),
-        (Vector(9, 9), 8),
-        (Vector(1, 2), 9),
-        (Vector(0, 1), 10),
-        (Vector(2, 0), 11),
-        (Vector(0, 0, 1), 12),
-        (Vector(4, 2), 13),
-        (Vector(9, 9, 9), 14),
-        (Vector(1, 2, 0), 15),
-        (Vector(2, 5), 16),
-        (Vector(1, 2, 1), 17),
-        (Vector(4, 2, 5), 18),
-        (Vector(2, 6, 9), 19),
-        (Vector(2, 1, 4), 20),
-        (Vector(5, 2), 21),
-        (Vector(2, 1), 22),
-        (Vector(2, 6), 23),
-        (Vector(2, 5, 2), 24),
-        (Vector(6, 9, 9), 25),
-        (Vector(2, 0, 1), 26),
-        (Vector(1, 4, 2), 27),
-        (Vector(6, 9), 28),
-        (Vector(0, 0), 29),
-        (Vector(5, 2, 6), 30),
-        (Vector(1, 4), 31)
+      trained.map(v => (v._1.map(_.toChar).mkString -> v._2.toInt)) == Vector(
+        ("2", 0),
+        ("9", 1),
+        ("6", 2),
+        ("1", 3),
+        ("4", 4),
+        ("5", 5),
+        ("0", 6),
+        ("012", 7),
+        ("99", 8),
+        ("12", 9),
+        ("01", 10),
+        ("201", 11),
+        ("25", 12),
+        ("269", 13),
+        ("121", 14),
+        ("999", 15),
+        ("142", 16),
+        ("21", 17),
+        ("252", 18),
+        ("14", 19),
+        ("52", 20),
+        ("00", 21),
+        ("425", 22),
+        ("26", 23),
+        ("120", 24),
+        ("699", 25),
+        ("526", 26),
+        ("20", 27),
+        ("214", 28),
+        ("42", 29),
+        ("69", 30),
+        ("001", 31)
       )
     )
   }
